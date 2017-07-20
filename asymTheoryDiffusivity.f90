db6247cb878d444f044cc31490e7aae6326df8e8
clvang <changlvang@gmail.com>
2017-07-20 11:34:04 -0700

    made lambda a floting point

--- asymTheoryDiffusivity.f90
+++ asymTheoryDiffusivity.f90
@@ -7,24 +7,26 @@ PROGRAM asymTheoryDiffusivity
 !  Record of revisions:
 !      Date       Programmer          Description of change
 !      ====       ==========          =====================
-!     07/19/17    C. Vang             Original code
+!     07/20/17    C. Vang             Original code
 !
 IMPLICIT NONE
 
 REAL :: dc_sq, K, do_measured, p, yo, d_c, y_ofc, percent_increase, &
 d_o, tau, LHS, epsilon, eps_ig, D, err_tol, Fx_sanity
-REAL, DIMENSION(7) :: fc_props
-CHARACTER(len=30) :: filename
+REAL, DIMENSION(6) :: fc_props
+CHARACTER(len=11) :: filename
 CHARACTER(len=100) :: junk
 INTEGER :: i, status
 
 !Get the file name and echo back to user
-WRITE(*,*) "Enter the name of the file with experimental measurements: "
-READ(*,22) filename
-22 FORMAT(A30)
+! WRITE(*,*) "Enter the name of the file with experimental measurements: "
+! READ(*,10) filename
+! 10 FORMAT(A30)
 
-WRITE(*,24) filename
-24 FORMAT(' ','The input file name is: ' A)
+filename = "fcprops.txt"
+
+WRITE(*,20) filename
+20 FORMAT(' ','The input file name is: ' A)
 
 !Open the file and check for errors on Open 
 OPEN(UNIT=1, FILE=filename, STATUS='OLD', ACTION='READ', IOSTAT=status)
@@ -36,18 +38,16 @@ openif : IF (status == 0) THEN
 	  READ(1,*,IOSTAT=status) junk
 	END DO readjunkLines
 
-	readData: DO i=1,7 			!read lines 5-9
+	readData: DO i=1,6 					!read lines 5-9
 	  READ(1,*,IOSTAT=status) fc_props(i)
 	END DO readData
 
-	p 			= fc_props(1)
-	dc_sq 		= fc_props(2)
-	K 			= fc_props(3)
-	do_measured = fc_props(4)
-	yo 			= fc_props(5)
-
-	eps_ig 		= fc_props(6)
-	err_tol 	= fc_props(7)
+	p 			= fc_props(1)  !chamber pressure [atm]
+	dc_sq 		= fc_props(2)  !drop diameter @ onset of fc squared [mm^2]
+	K 			= fc_props(3)  !burning rate constant prior to onset of fc
+	do_measured = fc_props(4)  !initial drop diameter measured [mm]
+	yo 			= fc_props(5)  !initial mass frac of low volitiliy component
+	err_tol 	= fc_props(6)  !error tolerrance for newton's method
 
 	CLOSE(UNIT=1)
 ELSE
@@ -69,17 +69,33 @@ d_o = do_measured * (1. + percent_increase) !do accounting for droplet swelling
 tau = LOG(d_o / d_c)
 LHS = y_ofc / yo
 
-CALL NewtonSolve(tau, LHS, eps_ig, epsilon, err_tol)
-WRITE(*,10) epsilon
-10 FORMAT("The value of epsilon is....................." ES14.6)
+!prompt user to enter initial guess to start Newton iteration
+WRITE(*,*) "Enter an initial guess for epsilon (0.01-0.8): "
+READ(*,*) eps_ig
 
-CALL Fx_eval(tau, epsilon, LHS, Fx_sanity)
-WRITE(*,15) Fx_sanity
-15 FORMAT("As a sanity check F(epsilon)=..............." ES14.6)
+Fx_sanity = 1.  !initialize sanity check
+
+DO WHILE ( ABS(Fx_sanity) > err_tol )
+
+	CALL NewtonSolve(tau, LHS, eps_ig, epsilon, err_tol)
+	WRITE(*,30) epsilon
+	30 FORMAT(" The value of epsilon is....................." ES14.6)
+
+	CALL Fx_eval(tau, epsilon, LHS, Fx_sanity)
+	WRITE(*,40) Fx_sanity
+	40 FORMAT(" As a sanity check F(epsilon)=..............." ES14.6)
+
+	IF ( ABS(Fx_sanity) > err_tol) THEN 
+		WRITE(*,*) "!! INITIAL GUESS IS TOO FAR AWAY. TRY DIFFERENT INITIAL GUESS !!"
+		WRITE(*,*) "Enter an initial guess for epsilon (0.01-0.8): "
+		READ(*,*) eps_ig	
+	END IF	
+
+END DO
 
 D = epsilon * K/ 8
-WRITE(*,20) D
-20 FORMAT("The effective liquid diffusivity is........." ES14.6, " mm^2/s")
+WRITE(*,50) D
+50 FORMAT(" The effective liquid diffusivity is........." ES14.6, " mm^2/s")
 
 END PROGRAM asymTheoryDiffusivity
 
@@ -99,7 +115,7 @@ SUBROUTINE NewtonSolve(tau, LHS, eps_ig, epsilon, err_tol)
 
 	INTEGER :: l, lmax, kmax, k 
 
-	eps = eps_ig
+	eps = eps_ig 		!set eps equal to initial guess
 	FTOL = err_tol		!max norm tolerance	for func iterats
 	XTOL = err_tol		!max norm tolerance for x iterates
 	kmax = 100			!max iteration
@@ -108,26 +124,33 @@ SUBROUTINE NewtonSolve(tau, LHS, eps_ig, epsilon, err_tol)
 	CALL Fx_eval(tau, eps, LHS, Fx)  			
 	CALL Dfx_eval(tau, eps, Dfx)
 
-	k    = 0		!initialize iteration counter
 	DX = 1			!initialize ||xk-xk-1||
 
-	!Compute eps using Newton's Method with damping
+	!Compute eps using Newton's Method with damping. Information on this
+	!particular method as well as some notation consistent with
+	!the variables used here can be found in Stoer and Bulirsch
 	IF (Fx == eps) THEN
-		WRITE(*,*) "Stopped. Initial guess for Antoinne Eqn Produces F(x)=0 exactly."
-		WRITE(*,30) eps
-		30 FORMAT("Initial guess: " ES14.6)
+		WRITE(*,*) "Stopped. Initial guess produces F(x)=0 exactly."
+		WRITE(*,60) eps
+		60 FORMAT("' ',Initial guess: " ES14.6)
 	ELSE
 		EuNormFx = SQRT( Fx**2 )  !initialize || fxk-fxk-1 ||
-		DO
+
+		newtonIterates : DO k = 0, kmax     
 			DxK = -Fx/Dfx
+
+			! WRITE(*,62) Fx, Dfx 
+			! 62 FORMAT("Fx  :" ES14.6, " Dfx: " ES14.6)
+			
 			!Monotiniticty Test 
 			Monotinicity : DO l=0,lmax
 				IF (k == 0) THEN
-					lambda = 1/(2**l)
+					lambda = 1./(2.**l)
 				ELSE
-					temp = 2*lambda/(2**l)
+					temp = 2.*lambda/(2.**l)
 					lambda = MIN(1.0, temp)
 				END IF
+
 				!Solve for DeltaXkBar
 				eps_bar = eps + lambda*DxK
 				CALL Fx_eval(tau, eps_bar, LHS, Fxbar)
@@ -141,14 +164,22 @@ SUBROUTINE NewtonSolve(tau, LHS, eps_ig, epsilon, err_tol)
 
 			eps_old = eps 
 			eps = eps + lambda*DxK 
+
 			DX = SQRT( (eps - eps_old)**2 )
 			CALL Fx_eval(tau, eps, LHS, Fx)  			
 			CALL Dfx_eval(tau, eps, Dfx)
-
 			EuNormFx = SQRT( (Fx)**2 )
+
+			! WRITE(*,64) lambda, DX, EuNormFx, DxK, eps, eps_old
+			! 64 FORMAT("lambda  :" ES14.6, " DX: " ES14.6, &
+			! 	"  EuNormFx:" ES14.6, "  DxK:" ES14.6, &
+			! 	"  eps:" ES14.6, "  eps_old:" ES14.6)
+			
 			IF ( (EuNormFx <= FTOL) .OR. (DX <= XTOL) .OR. (k > kmax) ) EXIT
-			k = k + 1
-		END DO
+			WRITE(*,70) k 
+			70 FORMAT(' ',"iteration number: " I3)
+
+		END DO newtonIterates
 	END IF 
 
 	epsilon = eps
@@ -195,6 +226,7 @@ SUBROUTINE Dfx_eval(tau, eps, Dfx)
 	REAL, INTENT(IN) :: tau, eps 
 	REAL, INTENT(OUT) :: Dfx
 
+	!be careful not to change anything here!
 	Dfx = EXP( -tau/(4*eps) ) * ( -2. * ( -EXP(tau/(4*eps))*SQRT(PI)*(6.+44*eps**2 + 9*tau) &
 		+ 9*eps*( SQRT(PI)*(4*eps + tau) + SQRT(tau/eps)*(2. + 4*eps + 3*tau ) ) &
 		+ 2*EXP( (tau/4.)*( 12.+(1./eps) ) )*SQRT(PI)*( 3. + 2*eps**2 * (-7. + 12*tau) ) ) & 
