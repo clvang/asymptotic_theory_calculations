24437006dd5da277d3cdd44a56ac583a433dc690
clvang <changlvang@gmail.com>
2017-07-20 19:55:55 -0700

    fixed linspace subroutine

--- asymTheoryDiffusivity.f90
+++ asymTheoryDiffusivity.f90
@@ -255,7 +255,6 @@ SUBROUTINE linspace(varArray, lowLimit, upLimit, numPts)
 	DO i = 2, numPts-1
 		varArray(i) = varArray(i-1) + intervalSize
 	END DO
-	varArray(1) = lowLimit
 	varArray(numPts) = upLimit
 END SUBROUTINE linspace
 
